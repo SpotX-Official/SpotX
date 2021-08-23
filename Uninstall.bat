8191c263de7f6c23a47bbf2b38e267bceb8064e2
amd64fox <62529699+amd64fox@users.noreply.github.com>
2021-08-23 10:15:45 +0300

    Update Uninstall.bat

--- Uninstall.bat
+++ Uninstall.bat
@@ -1,4 +1,5 @@
 @echo off
+SETLOCAL ENABLEDELAYEDEXPANSION
 echo *****************
 echo Author: @rednek46
 echo Modified: @Amd64fox
@@ -20,5 +21,40 @@ if exist "%APPDATA%\Spotify\Apps\xpui.bak" (
     move "%APPDATA%\Spotify\Apps\xpui.bak" "%APPDATA%\Spotify\Apps\xpui.spa" > NUL 2>&1
 ) 
 
-echo Deletion completed.
+if exist "%localappdata%\Spotify\Update" (
+    del /A:sr %localappdata%\Spotify\Update > NUL 2>&1
+) 
+
+
+if exist "%appdata%\Spotify\SpotifyMigrator.bak" (
+    rename %appdata%\Spotify\SpotifyMigrator.bak SpotifyMigrator.exe > NUL 2>&1
+) 
+
+if exist "%appdata%\Spotify\Spotify.vbs" (
+    del /f /s /q %appdata%\Spotify\Spotify.vbs > NUL 2>&1
+) 
+
+if exist "%appdata%\Spotify\cache-spotify.ps1" (
+    del /f /s /q %appdata%\Spotify\cache-spotify.ps1 > NUL 2>&1
+	del /f /s /q %USERPROFILE%\Desktop\Spotify.lnk > NUL 2>&1
+
+
+SET Esc_LinkDest=%USERPROFILE%\Desktop\Spotify.lnk
+SET Esc_LinkTarget=%appdata%\Spotify\Spotify.exe
+SET Esc_WorkLinkTarget=%appdata%\Spotify\
+SET cSctVBS=CreateShortcut.vbs
+((
+echo Set oWS = WScript.CreateObject^("WScript.Shell"^) 
+echo sLinkFile = oWS.ExpandEnvironmentStrings^("!Esc_LinkDest!"^)
+echo Set oLink = oWS.CreateShortcut^(sLinkFile^) 
+echo oLink.TargetPath = oWS.ExpandEnvironmentStrings^("!Esc_LinkTarget!"^)
+echo oLink.WorkingDirectory = oWS.ExpandEnvironmentStrings^("!Esc_WorkLinkTarget!"^)
+echo oLink.Save
+)1>!cSctVBS!
+cscript !cSctVBS!
+DEL !cSctVBS! /f /q
+)
+) 
+
+echo Patch successfully removed
 pause

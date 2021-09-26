da4def8d47a906aa951a76f0fe92be8a81ff47d9
amd64fox <62529699+amd64fox@users.noreply.github.com>
2021-09-26 16:24:01 +0300

    Update Install.bat

--- Install.bat
+++ Install.bat
@@ -1,4 +1,4 @@
 @echo off
-powershell -Command "& {[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12}"; "& {Invoke-WebRequest -UseBasicParsing 'https://raw.githubusercontent.com/amd64fox/SpotX/main/install.ps1' | Invoke-Expression}"
+powershell -Command "& {[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12}"; "& {Invoke-WebRequest -UseBasicParsing 'https://raw.githubusercontent.com/amd64fox/SpotX/main/Install.ps1' | Invoke-Expression}"
 pause
 exit

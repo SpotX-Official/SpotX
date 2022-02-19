af2afe9598d7745fd2492154e63d66733922ae79
amd64fox <amd64fox1@gmail.com>
2022-02-19 06:18:20 +0300

    powershell and curl check
    Added powershell and curl checks before installation

--- Install_Rus.bat
+++ Install_Rus.bat
@@ -1,45 +1,77 @@
 @echo off
 Setlocal EnableDelayedExpansion
+set "exec=powershell $PSVersionTable.PSVersion.major"
+for /f %%i in ('%exec%') do set ps-v=%%i 2>nul
 chcp 65001 >nul
+if  %ps-v% LEQ 2 (
+    echo Powershell %ps-v%не поддерживается
+    @echo.
+    echo Пожалуйста, прочитайте инструкцию "Устаревшие версии PowerShell" по следующей ссылке
+    @echo.
+    echo "https://4pda.to/forum/index.php?act=findpost&pid=104279894&anchor=outdated_versions"
+    @echo.
+    pause
+    exit
+)
+curl -V >nul 2>&1
+if %errorlevel% EQU 9009 (
+    echo Команда "Curl" отсутствует в системе
+    @echo.
+    echo Пожалуйста, перейдите по следующей ссылке для ручной установки "Curl"
+    @echo.
+    echo "http://www.confusedbycode.com/curl/#downloads"
+    @echo.
+    pause
+    exit
+)
 for /f %%i in ('curl --write-out %%{http_code} --silent --output /dev/null --insecure https://raw.githubusercontent.com/amd64fox/SpotX/main/Install_rus.ps1') do set STATUS_CODE=%%i 2>nul
 if %STATUS_CODE% EQU 200 (
-chcp 866 >nul
-powershell -Command "& {[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12}"; "& {(Invoke-WebRequest -UseBasicParsing 'https://raw.githubusercontent.com/amd64fox/SpotX/main/Install_rus.ps1').Content | Invoke-Expression}"
+    chcp 866 >nul
+    powershell -Command "& {[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12}"; "& {(Invoke-WebRequest -UseBasicParsing 'https://raw.githubusercontent.com/amd64fox/SpotX/main/Install_rus.ps1').Content | Invoke-Expression}"
 )
 if %STATUS_CODE% LSS 1 (
-echo Нет подключения к сети
-echo Попытка повторного подключения через ...
-TIMEOUT /T 5
-cls
-for /f %%i in ('curl --write-out %%{http_code} --silent --output /dev/null --insecure https://raw.githubusercontent.com/amd64fox/SpotX/main/Install_rus.ps1') do set STATUS_CODE=%%i 2>nul
-if "!STATUS_CODE!" EQU "200 " (
-cls
-chcp 866 >nul
-powershell -Command "& {[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12}"; "& {(Invoke-WebRequest -UseBasicParsing 'https://raw.githubusercontent.com/amd64fox/SpotX/main/Install_rus.ps1').Content | Invoke-Expression}"
-) else ( 
-echo Снова ошибка 
-echo Проверьте свое интернет соединение
-echo Скрипт остановлен
-pause
-exit
-)
+    echo Нет подключения к сети
+    @echo.
+    echo Попытка повторного подключения через ...
+    TIMEOUT /T 5
+    cls
+    for /f %%i in ('curl --write-out %%{http_code} --silent --output /dev/null --insecure https://raw.githubusercontent.com/amd64fox/SpotX/main/Install_rus.ps1') do set STATUS_CODE=%%i 2>nul
+    if "!STATUS_CODE!" EQU "200 " (
+        cls
+        chcp 866 >nul
+        powershell -Command "& {[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12}"; "& {(Invoke-WebRequest -UseBasicParsing 'https://raw.githubusercontent.com/amd64fox/SpotX/main/Install_rus.ps1').Content | Invoke-Expression}"
+    ) else ( 
+        echo Снова ошибка 
+        @echo.
+        echo Проверьте свое интернет соединение
+        @echo.
+        echo Скрипт остановлен
+        @echo.
+        pause
+        exit
+    )
 )
 if %STATUS_CODE% GTR 100 if not %STATUS_CODE% EQU 200 (
-echo Не удалось подключиться к github.com
-echo Код ответа HTTP %STATUS_CODE%
-echo Попытка повторного подключения через ...
-TIMEOUT /T 5
-cls
-for /f %%i in ('curl --write-out %%{http_code} --silent --output /dev/null --insecure https://raw.githubusercontent.com/amd64fox/SpotX/main/Install_rus.ps1') do set STATUS_CODE=%%i 2>nul
-if "!STATUS_CODE!" EQU "200 " (
-cls
-chcp 866 >nul
-powershell -Command "& {[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12}"; "& {(Invoke-WebRequest -UseBasicParsing 'https://raw.githubusercontent.com/amd64fox/SpotX/main/Install_rus.ps1').Content | Invoke-Expression}"
-) else (
-echo Снова ошибка
-echo Попробуйте запустить спустя некоторое время 
-echo Скрипт остановлен
-)
+    echo Не удалось подключиться к github.com
+    @echo.
+    echo Код ответа HTTP %STATUS_CODE%
+    @echo.
+    echo Попытка повторного подключения через ...
+    TIMEOUT /T 5
+    cls
+    for /f %%i in ('curl --write-out %%{http_code} --silent --output /dev/null --insecure https://raw.githubusercontent.com/amd64fox/SpotX/main/Install_rus.ps1') do set STATUS_CODE=%%i 2>nul
+    if "!STATUS_CODE!" EQU "200 " (
+        cls
+        chcp 866 >nul
+        powershell -Command "& {[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12}"; "& {(Invoke-WebRequest -UseBasicParsing 'https://raw.githubusercontent.com/amd64fox/SpotX/main/Install_rus.ps1').Content | Invoke-Expression}"
+    ) else (
+        echo Снова ошибка
+        @echo.
+        echo Попробуйте запустить спустя некоторое время
+        @echo.
+        echo Скрипт остановлен
+        @echo.
+    )
 )
 pause
 exit

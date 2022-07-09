a99bf71e88e487dfae5374a1af08cdfc58dfe550
amd64fox <amd64fox1@gmail.com>
2022-07-09 06:17:26 +0300

    Revert "script run error"
    This reverts commit c8e50f67fbbc53dc185343ab9e6f4f99399ecf87.

--- Install.bat
+++ Install.bat
@@ -25,7 +25,7 @@ if %errorlevel% EQU 9009 (
 )
 for /f %%i in ('curl --write-out %%{http_code} --silent --output /dev/null --insecure https://raw.githubusercontent.com/amd64fox/SpotX/main/Install.ps1') do set STATUS_CODE=%%i 2>nul
 if %STATUS_CODE% EQU 200 (
-    powershell -Command "&{[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12}; """"& { $((Invoke-WebRequest -UseBasicParsing 'https://raw.githubusercontent.com/amd64fox/SpotX/main/Install.ps1').Content)} """" | Invoke-Expression"
+    powershell -Command "& {[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12}"; "& {(Invoke-WebRequest -UseBasicParsing 'https://raw.githubusercontent.com/amd64fox/SpotX/main/Install.ps1').Content | Invoke-Expression}"
 )
 if %STATUS_CODE% LSS 1 (
     echo No network connection
@@ -36,7 +36,7 @@ if %STATUS_CODE% LSS 1 (
     for /f %%i in ('curl --write-out %%{http_code} --silent --output /dev/null --insecure https://raw.githubusercontent.com/amd64fox/SpotX/main/Install.ps1') do set STATUS_CODE=%%i 2>nul
     if "!STATUS_CODE!" EQU "200 " (
         cls
-        powershell -Command "&{[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12}; """"& { $((Invoke-WebRequest -UseBasicParsing 'https://raw.githubusercontent.com/amd64fox/SpotX/main/Install.ps1').Content)} """" | Invoke-Expression"
+        powershell -Command "& {[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12}"; "& {(Invoke-WebRequest -UseBasicParsing 'https://raw.githubusercontent.com/amd64fox/SpotX/main/Install.ps1').Content | Invoke-Expression}"
     ) else ( 
         echo Error again
         @echo.
@@ -59,7 +59,7 @@ if %STATUS_CODE% GTR 100 if not %STATUS_CODE% EQU 200 (
     for /f %%i in ('curl --write-out %%{http_code} --silent --output /dev/null --insecure https://raw.githubusercontent.com/amd64fox/SpotX/main/Install.ps1') do set STATUS_CODE=%%i 2>nul
     if "!STATUS_CODE!" EQU "200 " (
         cls
-        powershell -Command "&{[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12}; """"& { $((Invoke-WebRequest -UseBasicParsing 'https://raw.githubusercontent.com/amd64fox/SpotX/main/Install.ps1').Content)} """" | Invoke-Expression"
+        powershell -Command "& {[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12}"; "& {(Invoke-WebRequest -UseBasicParsing 'https://raw.githubusercontent.com/amd64fox/SpotX/main/Install.ps1').Content | Invoke-Expression}"
     ) else (
         echo Error again
         @echo.

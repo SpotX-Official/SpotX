6af13ad2d454af29deaf2a7cb73ee8f475501d5d
CDzungx <59353870+CDzungx@users.noreply.github.com>
2022-10-03 19:31:30 +0700

    Update Install.bat
    Update for translation

--- Install.bat
+++ Install.bat
@@ -1,7 +1,8 @@
 @echo off
 
-IF EXIST "%ProgramFiles%\PowerShell\7\pwsh.exe" ("%ProgramFiles%\PowerShell\7\pwsh.exe" -Command "& {[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12}"; "& {(Invoke-WebRequest -UseBasicParsing 'https://raw.githubusercontent.com/amd64fox/SpotX/main/Install.ps1').Content | Invoke-Expression}")
-ELSE (%SYSTEMROOT%\System32\WindowsPowerShell\v1.0\powershell.exe -Command "& {[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12}"; "& {(Invoke-WebRequest -UseBasicParsing 'https://raw.githubusercontent.com/amd64fox/SpotX/main/Install.ps1').Content | Invoke-Expression}")
+IF EXIST "%ProgramFiles%\PowerShell\7\pwsh.exe" (
+         "%ProgramFiles%\PowerShell\7\pwsh.exe" -Command "& {[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12; (iwr -useb 'https://raw.githubusercontent.com/amd64fox/SpotX/main/Install.ps1').Content | iex}"
+  ) ELSE (%SYSTEMROOT%\System32\WindowsPowerShell\v1.0\powershell.exe -Command "& {[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12}"; "& {(Invoke-WebRequest -UseBasicParsing 'https://raw.githubusercontent.com/amd64fox/SpotX/main/Install.ps1').Content | Invoke-Expression}")
 
 pause
 exit /b

e8b082b227e86b9d3406f32bcc4579d5b16b62f7
Gamer Snave <snaveqiptoo@gmail.com>
2023-04-21 22:49:53 +0300

    Updated error message when unable to resolve remote name using ping method

--- Install_New_theme.bat
+++ Install_New_theme.bat
@@ -1,5 +1,14 @@
 @echo off
 
+ping www.github.com >nul 2>&1
+if %errorlevel% neq 0 (
+  color 0C 
+  echo Please Connect to the Internet First.
+  pause
+  color 07 
+  exit /b 1
+)
+
 %SYSTEMROOT%\System32\WindowsPowerShell\v1.0\powershell.exe -Command "&{[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12}; """"& { $((Invoke-WebRequest -UseBasicParsing 'https://raw.githubusercontent.com/amd64fox/SpotX/main/Install.ps1').Content)} -new_theme """" | Invoke-Expression"
 
 pause

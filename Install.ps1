522ced1a713dd3b1386a8510679e66e7d4655cea
Sharun <sharunksplus@gmail.com>
2023-06-02 19:31:06 -0400

    added validations / auto-complete for lyrics_stat param

--- Install.ps1
+++ Install.ps1
@@ -59,6 +59,7 @@ param
     [switch]$no_shortcut,
 
     [Parameter(HelpMessage = 'Static color for lyrics.')]
+    [ValidateSet('default', 'red', 'orange', 'yellow', 'spotify', 'blue', 'purple', 'strawberry', 'pumpkin', 'sandbar', 'radium', 'oceano', 'royal', 'github', 'discord', 'drot', 'forest', 'fresh', 'zing', 'pinkle', 'krux', 'blueberry', 'postlight')]
     [string]$lyrics_stat,
 
     [Parameter(HelpMessage = 'Accumulation of track listening history with Goofy.')]

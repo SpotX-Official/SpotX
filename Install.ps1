3e98374b7df09810c1a24d171ca52af84365becf
amd64fox <62529699+amd64fox@users.noreply.github.com>
2023-08-13 04:54:44 +0300

    version bump to 1.2.18.997
    - added exp which updates the sorting for the left sidebar
    - russian translation updated

--- Install.ps1
+++ Install.ps1
@@ -337,7 +337,7 @@ if (!($version -and $version -match $match_v)) {
     }
     else {  
         # Recommended version for Win 10-12
-        $onlineFull = "1.2.17.834.g26ee1129-632" 
+        $onlineFull = "1.2.18.997.g167085cf-254" 
     }
 }
 else {

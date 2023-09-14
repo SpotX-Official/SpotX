0bbf4193e80bb04bd88d9f9426461ea3a46449d4
amd64fox <62529699+amd64fox@users.noreply.github.com>
2023-09-14 13:21:48 +0300

    version bump to 1.2.20.1214

--- Install.ps1
+++ Install.ps1
@@ -335,7 +335,7 @@ if (!($version -and $version -match $match_v)) {
     }
     else {  
         # Recommended version for Win 10-12
-        $onlineFull = "1.2.20.1210.g2a8a8a57-590" 
+        $onlineFull = "1.2.20.1214.g009959bd-963" 
     }
 }
 else {

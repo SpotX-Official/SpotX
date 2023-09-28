16d99113da53077af708d332afad4fb2957d172b
amd64fox <62529699+amd64fox@users.noreply.github.com>
2023-09-28 23:29:16 +0300

    Install.ps1 to run.ps1

--- Install_New_theme.bat
+++ Install_New_theme.bat
@@ -1,6 +1,6 @@
 @echo off
 
-%SYSTEMROOT%\System32\WindowsPowerShell\v1.0\powershell.exe -Command "&{[Net.ServicePointManager]::SecurityProtocol = 3072}; """"& { $(Invoke-WebRequest -UseBasicParsing 'https://spotx-official.github.io/SpotX/Install.ps1')} -new_theme """" | Invoke-Expression"
+%SYSTEMROOT%\System32\WindowsPowerShell\v1.0\powershell.exe -Command "&{[Net.ServicePointManager]::SecurityProtocol = 3072}; """"& { $(Invoke-WebRequest -UseBasicParsing 'https://spotx-official.github.io/run.ps1')} -new_theme """" | Invoke-Expression"
 
 pause
 exit /b

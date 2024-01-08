7135a6a3ecc0cece9b87398d53b47e9686d2c7a3
amd64fox <62529699+amd64fox@users.noreply.github.com>
2024-01-08 21:22:39 +0300

    backup url added
    #548

--- scripts/Install_Auto.bat
+++ scripts/Install_Auto.bat
@@ -1,6 +1,14 @@
 @echo off
 
-%SYSTEMROOT%\System32\WindowsPowerShell\v1.0\powershell.exe -Command "&{[Net.ServicePointManager]::SecurityProtocol = 3072}; """"& { $(Invoke-WebRequest -UseBasicParsing 'https://spotx-official.github.io/run.ps1')} -confirm_uninstall_ms_spoti -confirm_spoti_recomended_over -podcasts_off -block_update_on -start_spoti -new_theme -adsections_off -lyrics_stat spotify """" | Invoke-Expression"
+:: Line for changing spotx parameters, each parameter should be separated by a space
+set param=-confirm_uninstall_ms_spoti -confirm_spoti_recomended_over -podcasts_off -block_update_on -start_spoti -new_theme -adsections_off -lyrics_stat spotify
+
+set url='https://raw.githubusercontent.com/SpotX-Official/spotx-official.github.io/main/run.ps1'
+set url2='https://spotx-official.github.io/run.ps1'
+set tls=[Net.ServicePointManager]::SecurityProtocol = [Net.ServicePointManager]::SecurityProtocol -bor [Net.SecurityProtocolType]::Tls12;
+
+%SYSTEMROOT%\System32\WindowsPowerShell\v1.0\powershell.exe ^
+-Command %tls% $p='%param%'; """ & { $(try { iwr -useb %url% } catch { $p+= ' -m'; iwr -useb %url2% })} $p """" | iex
 
 pause
 exit /b

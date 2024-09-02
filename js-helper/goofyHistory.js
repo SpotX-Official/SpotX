fd47891b553533db2b3cbec0e568c183de7b3468
amd64fox <62529699+amd64fox@users.noreply.github.com>
2024-09-02 22:40:16 +0300

    Create goofyHistory.js

---
+++ js-helper/goofyHistory.js
@@ -0,0 +1,20 @@
+const unique = new Set();
+
+function Gofy_history(e, urlForm, idBox) {
+
+    const uri = e?.item?.uri;
+
+    if (uri && uri.includes('spotify:track:') && !unique.has(uri)) {
+
+        unique.add(uri);
+
+        fetch(urlForm, {
+            "headers": {
+                "content-type": "application/x-www-form-urlencoded",
+            },
+            "body": "entry." + idBox + "=" + uri,
+            "method": "POST",
+            "mode": "no-cors",
+        }).catch(error => console.error('error sending uri to google form:', error));
+    }
+}

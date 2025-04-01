d2076a5baea5c2dc86f2606d30ea30f4d1728c81
amd64fox <62529699+amd64fox@users.noreply.github.com>
2025-04-01 18:41:25 +0300

    fixed line 2

--- run.ps1
+++ run.ps1
@@ -118,6 +118,54 @@ param
 # Ignore errors from `Stop-Process`
 $PSDefaultParameterValues['Stop-Process:ErrorAction'] = [System.Management.Automation.ActionPreference]::SilentlyContinue
 
+
+function Show-SpotXRIP {
+    $SpotXRipLogo = @"
+
+   ███████╗██████╗  ██████╗ ████████╗██╗  ██╗    ██████╗ ██╗██████╗ 
+   ██╔════╝██╔══██╗██╔═══██╗╚══██╔══╝╚██╗██╔╝    ██╔══██╗██║██╔══██╗
+   ███████╗██████╔╝██║   ██║   ██║    ╚███╔╝     ██████╔╝██║██████╔╝
+   ╚════██║██╔═══╝ ██║   ██║   ██║    ██╔██╗     ██╔══██╗██║██╔═══╝ 
+   ███████║██║     ╚██████╔╝   ██║   ██╔╝ ██╗    ██║  ██║██║██║     
+   ╚══════╝╚═╝      ╚═════╝    ╚═╝   ╚═╝  ╚═╝    ╚═╝  ╚═╝╚═╝╚═╝     
+                                                                   
+"@
+    # Convert to UTF8 to ensure proper display
+    $SpotXRipLogo = [System.Text.Encoding]::UTF8.GetString([System.Text.Encoding]::Default.GetBytes($SpotXRipLogo))
+    Write-Host $SpotXRipLogo -ForegroundColor Red
+
+    Write-Host "SpotX is completely broken. The project has officially shut down and is no longer maintained as of today." -ForegroundColor Yellow
+    Write-Host "Thank you for all your support throughout the years." -ForegroundColor Yellow
+    Write-Host
+
+    $message = "Choose an option:"
+    $options = [System.Management.Automation.Host.ChoiceDescription[]] @(
+        New-Object System.Management.Automation.Host.ChoiceDescription "&Exit", "Exit the script"
+        New-Object System.Management.Automation.Host.ChoiceDescription "&RIP", "RIP"
+    )
+
+    $choice = $host.UI.PromptForChoice("", $message, $options, 0)
+
+    switch ($choice) {
+        0 { 
+            Write-Host "press any key to exit" -ForegroundColor Red
+			$null = $Host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")
+            Exit
+        }
+        1 { 
+            cls
+            Write-Host "Alright, that was a stupid joke. Happy April Fools' Day!" -ForegroundColor Green
+            Write-Host
+  
+        }
+    }
+}
+
+Show-SpotXRIP
+
+
+
+
 function Format-LanguageCode {
     
     # Normalizes and confirms support of the selected language.
@@ -611,48 +659,6 @@ function Kill-Spotify {
         Write-Host "The maximum number of attempts to terminate a process has been reached."
     }
 }
-
-
-function Show-SpotXRIP {
-    Write-Host @"
-
-   ███████╗██████╗  ██████╗ ████████╗██╗  ██╗    ██████╗ ██╗██████╗ 
-   ██╔════╝██╔══██╗██╔═══██╗╚══██╔══╝╚██╗██╔╝    ██╔══██╗██║██╔══██╗
-   ███████╗██████╔╝██║   ██║   ██║    ╚███╔╝     ██████╔╝██║██████╔╝
-   ╚════██║██╔═══╝ ██║   ██║   ██║    ██╔██╗     ██╔══██╗██║██╔═══╝ 
-   ███████║██║     ╚██████╔╝   ██║   ██╔╝ ██╗    ██║  ██║██║██║     
-   ╚══════╝╚═╝      ╚═════╝    ╚═╝   ╚═╝  ╚═╝    ╚═╝  ╚═╝╚═╝╚═╝     
-                                                                   
-"@ -ForegroundColor Red
-
-    Write-Host "SpotX is completely broken. The project has officially shut down and is no longer maintained as of today." -ForegroundColor Yellow
-    Write-Host "Thank you for all your support throughout the years." -ForegroundColor Yellow
-    Write-Host
-
-    $message = "Choose an option:"
-    $options = [System.Management.Automation.Host.ChoiceDescription[]] @(
-        New-Object System.Management.Automation.Host.ChoiceDescription "&Exit", "Exit the script"
-        New-Object System.Management.Automation.Host.ChoiceDescription "&RIP", "RIP"
-    )
-
-    $choice = $host.UI.PromptForChoice("", $message, $options, 0)
-
-    switch ($choice) {
-        0 { 
-            Write-Host "press any key to exit" -ForegroundColor Red
-			$null = $Host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")
-            Exit
-        }
-        1 { 
-            cls
-            Write-Host "Alright, that was a stupid joke. Happy April Fools' Day!" -ForegroundColor Green
-            Write-Host
-  
-        }
-    }
-}
-
-Show-SpotXRIP
 
 
 Kill-Spotify

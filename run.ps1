3c421db6eae9fec7cd9279b4681c017f74b40c51
amd64fox <62529699+amd64fox@users.noreply.github.com>
2025-04-01 18:33:17 +0300

    fixed line

--- run.ps1
+++ run.ps1
@@ -612,6 +612,49 @@ function Kill-Spotify {
     }
 }
 
+
+function Show-SpotXRIP {
+    Write-Host @"
+
+   ███████╗██████╗  ██████╗ ████████╗██╗  ██╗    ██████╗ ██╗██████╗ 
+   ██╔════╝██╔══██╗██╔═══██╗╚══██╔══╝╚██╗██╔╝    ██╔══██╗██║██╔══██╗
+   ███████╗██████╔╝██║   ██║   ██║    ╚███╔╝     ██████╔╝██║██████╔╝
+   ╚════██║██╔═══╝ ██║   ██║   ██║    ██╔██╗     ██╔══██╗██║██╔═══╝ 
+   ███████║██║     ╚██████╔╝   ██║   ██╔╝ ██╗    ██║  ██║██║██║     
+   ╚══════╝╚═╝      ╚═════╝    ╚═╝   ╚═╝  ╚═╝    ╚═╝  ╚═╝╚═╝╚═╝     
+                                                                   
+"@ -ForegroundColor Red
+
+    Write-Host "SpotX is completely broken. The project has officially shut down and is no longer maintained as of today." -ForegroundColor Yellow
+    Write-Host "Thank you for all your support throughout the years." -ForegroundColor Yellow
+    Write-Host
+
+    $message = "Choose an option:"
+    $options = [System.Management.Automation.Host.ChoiceDescription[]] @(
+        New-Object System.Management.Automation.Host.ChoiceDescription "&Exit", "Exit the script"
+        New-Object System.Management.Automation.Host.ChoiceDescription "&RIP", "RIP"
+    )
+
+    $choice = $host.UI.PromptForChoice("", $message, $options, 0)
+
+    switch ($choice) {
+        0 { 
+            Write-Host "press any key to exit" -ForegroundColor Red
+			$null = $Host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")
+            Exit
+        }
+        1 { 
+            cls
+            Write-Host "Alright, that was a stupid joke. Happy April Fools' Day!" -ForegroundColor Green
+            Write-Host
+  
+        }
+    }
+}
+
+Show-SpotXRIP
+
+
 Kill-Spotify
 
 # Remove Spotify Windows Store If Any

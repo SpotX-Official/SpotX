6b586d46aca72de00b33d1e0def34905fcb7fd94
amd64fox <62529699+amd64fox@users.noreply.github.com>
2025-10-18 14:33:39 +0300

    version bump to 1.2.75.506

--- run.ps1
+++ run.ps1
@@ -382,7 +382,7 @@ if (!($version -and $version -match $match_v)) {
     }
     else {  
         # latest tested version for Win 10-12 
-        $onlineFull = "1.2.75.499.g0c61121f-236"
+        $onlineFull = "1.2.75.506.g38295b74-355"
     }
 }
 else {

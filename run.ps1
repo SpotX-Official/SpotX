d2f652b73e00481a0613edfd85c6fee31c09a412
amd64fox <62529699+amd64fox@users.noreply.github.com>
2026-01-14 03:16:22 +0300

    version bump to 1.2.81.264

--- run.ps1
+++ run.ps1
@@ -416,7 +416,7 @@ if (!($version -and $version -match $match_v)) {
     }
     else {  
         # latest tested version for Win 10-12 
-        $onlineFull = "1.2.80.349.g2efc88b5-968"
+        $onlineFull = "1.2.81.264.gb4ad4efa-1866"
     }
 }
 else {
@@ -1235,7 +1235,7 @@ function Helper($paramname) {
                     } 
                 }
             }
-            if (!$premium) { Remove-Json -j $Enable -p 'RemoteDownloads', 'Magpie', 'MagpiePrompting', 'MagpieScheduling' }
+            if (!$premium) { Remove-Json -j $Enable -p 'RemoteDownloads', 'Magpie', 'MagpiePrompting', 'MagpieScheduling', 'MagpieCuration' }
 
             # Disable unimportant exp
             if ($exp_spotify) {

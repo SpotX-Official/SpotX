e22aba4ed47e6335209791a9fe95bed4923091aa
amd64fox <62529699+amd64fox@users.noreply.github.com>
2026-07-11 16:37:12 +0300

    version bump to 1.2.94

--- run.ps1
+++ run.ps1
@@ -2,7 +2,7 @@
 param
 (
     [Parameter(HelpMessage = 'Latest recommended Spotify version for Windows 10+.')]
-    [string]$latest_full = "1.2.93",
+    [string]$latest_full = "1.2.94",
 
     [Parameter(HelpMessage = 'Latest supported Spotify version for Windows 7-8.1')]
     [string]$last_win7_full = "1.2.5.1006.g22820f93",
